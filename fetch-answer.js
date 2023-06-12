e4548e0797c515f2e04d5593ac537fdd98df11cf
sarababich <94055284+sarababich@users.noreply.github.com>
2023-06-12 22:21:34 +0200

    Add files via upload

---
+++ fetch-answer.js
@@ -0,0 +1,32 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const ul = document.getElementById('quiz');
+
+
+const url = 'http://localhost:3000/api/quiz';
+
+//const url = 'data/data.json';
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data.quizQuestion);
+        let question = data.quizQuestion;
+        return quizQuestion.map(function(question)
+        {
+            let li = createNode('li');
+            li.innerHTML = question.quizQuestion + question.quizAnswer;
+            append(ul, li);
+        })
+    })
+
+
+
+    .catch(function(error) {
+        console.log(error);
+    });
